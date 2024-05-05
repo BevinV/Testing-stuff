4f701280a676df4f3cf1a5a484784547b0b4ab4b
Your Name <alisterblack007@gmail.com>
2024-05-05 09:47:08 +0530

    New shit dialgue

--- mic.py
+++ mic.py
@@ -1,2 +1,4 @@
 print("This is batman speakinh")
 print("Gotham is always safe")
+
+print("You've merely adopted the darkness while i was born in it, molded by it")
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 print("hello testy")
+
+print("The world is coming to shit and all you're thinking about is cheetos wht nonsense")
